44d849b30d88d9e2bab0390cadefb9ee8b364a6d
zakirzakuman-rgb <zakirzakuman@gmail.com>
2026-03-17 20:13:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let currentUser = "";
+
+function checkLogin() {
+    let name = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+
+    // ፓስዎርዱ እዚህ ጋር ይወሰናል
+    if (name.trim() !== "" && pass === "zakir123") {
+        currentUser = name;
+        
+        // የሎጊን ገጹን ደብቅ
+        document.getElementById("login-page").style.display = "none";
+        
+        // የጥያቄ ገጹን አሳይ
+        document.getElementById("main-quiz-page").style.display = "block";
+        
+        // እንኳን መጣህ መልዕክት
+        document.getElementById("welcome-text").innerText = "Welcome, " + currentUser + "! Ready for the challenge?";
+    } else {
+        alert("Incorrect Username or Password!");
+    }
+}
+
+
 function startQuiz(subject) {
     let questions = [];
 
